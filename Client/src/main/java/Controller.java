6e83c07138c422d10f4c4e228c3f63901a7b2a3b
Haidz97 <makc.obuho@mail.ru>
2025-06-02 18:54:37 +0300

    Реализован логер по инициализации интерфейса, получении айпи, указании данных, подключения сервера, обработки ответа от сервера и различные ошибки по доустпам, обменам данных и тд.

--- Client/src/main/java/Controller.java
+++ Client/src/main/java/Controller.java
@@ -1,4 +1,7 @@
-import ClientServers.ClServ;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.clientserver.common.ClServ;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -13,6 +16,9 @@ import java.net.UnknownHostException;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
+    private static final Logger logger = LoggerFactory.getLogger(Controller.class); // Логгер класса
+
     public ImageView im;
     public TextField fsity;
     public Label name;
@@ -21,18 +27,31 @@ public class Controller implements Initializable {
     public String response;
     public String request;
     public String IP;
+    public String ip;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        logger.info("Инициализация интерфейса.");
+
         im.setImage(new Image("image.png"));
         getIP();
+
+        logger.debug("Полученный IP: {}", ip);
+
         name.setText(request);
         temp.setText(response);
     }
 
     public void clOk(ActionEvent actionEvent) {
         sity = fsity.getText();
+
+        if(sity.isEmpty()) { // Проверка поля ввода города
+            logger.warn("Город не указан!");
+            return;
+        }
+
         init();
         name.setText(request);
         temp.setText(response);
@@ -41,15 +60,22 @@ public class Controller implements Initializable {
     public void init() {
 
         try (ClServ module = new ClServ(IP, 2654)) {
+
+            logger.info("Подключение к серверу успешно установлено.");
+
             System.out.println("Connected to server");
             request = sity;
             module.writeLine(request);
             request = module.readerLine();
             response = module.readerLine();
+
+            logger.info("Запрос обработан. Ответ сервера: {}", response);
+
             System.out.println("" + response);
 
         } catch (IOException e) {
             e.printStackTrace();
+            logger.error("Ошибка при обмене данными с сервером.", e);
         }
     }
 
@@ -59,6 +85,7 @@ public class Controller implements Initializable {
             myIP = InetAddress.getLocalHost();
         } catch (UnknownHostException e) {
             System.out.println(" ошибка доступа ->" + e);
+            logger.error("Ошибка получения локального IP адреса.", e);
         }
         IP =  myIP.getHostAddress();
         return IP;

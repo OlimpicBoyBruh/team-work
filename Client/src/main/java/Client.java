5f8f872eda8f9c85a634eaa429cc5314942bf6de
Haidz97 <makc.obuho@mail.ru>
2025-06-02 19:00:44 +0300

    Реализован логер по инициализации интерфейса, получении айпи, указании данных, подключения сервера, обработки ответа от сервера и различные ошибки по доустпам, обменам данных и тд.

--- Client/src/main/java/Client.java
+++ Client/src/main/java/Client.java
@@ -1,3 +1,5 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
@@ -7,16 +9,24 @@ import javafx.stage.Stage;
 
 public class Client extends Application {
 
+    private static final Logger logger = LoggerFactory.getLogger(Client.class);
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+        logger.info("Начало инициализации окна приложения.");
+
         Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("Гидрометцентр");
         primaryStage.setScene(new Scene(root, 550, 400));
         primaryStage.show();
+
+        logger.info("Окно приложения отображено.");
     }
 
 
     public static void main(String[] args) {
+
+        logger.info("Запуск приложения...");
         launch(args);
     }
 
